af4fa9ef801239ac884bfa1ca2f685c6f82cf363
USER <USER@DESKTOP-TJSJ0HQ>
2023-04-10 00:34:02 -0500

    actualizacion general

---
+++ src/main/java/pea2/onpe/LpwaEa2CalderonNievaApplication.java
@@ -0,0 +1,13 @@
+package pea2.onpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpwaEa2CalderonNievaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpwaEa2CalderonNievaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pea2/onpe/services/IGrupoVotacionService.java
@@ -0,0 +1,7 @@
+package pea2.onpe.services;
+
+import pea2.onpe.model.GrupoVotacion;
+
+public interface IGrupoVotacionService {
+	public GrupoVotacion  getGrupoVotacion(String id );
+}
